7fc3a0bdabb7ccb7633937cd9885e6c6939735bd
Night5word <afga93+git@gmail.com>
2017-03-06 15:37:34 +0100

    player object

---
+++ player.py
@@ -0,0 +1,19 @@
+"""
+    player object and methods
+"""
+
+class Player(object):
+    """represents a player"""
+
+    def __init__(self, health=14, effects=None):
+        self.health = health
+        self._left_hand = []
+        self._right_hand = []
+        if effects:
+            self.effects = effects
+        else:
+            self.effects = {}
+
+    def get_hands(turns):
+        """"""
+        
